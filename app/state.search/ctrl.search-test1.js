d48a525e9d9b82dadb585de9dff5634c0d58f799
Julius Alvarado <javascript.uiux@gmail.com>
2017-12-18 18:18:37 -0800

    got a super basic 'search results' to show on keyUp but when user deletes search query the results DON'T become hidden again

--- app/state.search/ctrl.search-test1.js
+++ app/state.search/ctrl.search-test1.js
@@ -10,12 +10,12 @@
             $scope.query = '';
             $scope.dataSet1 = [
                 {firstname: 'justin', lastname: 'smith', date: '1-1-2018'},
-                {firstname: 'julissa', lastname: 'sanchez', date: '1-1-2018'},
-                {firstname: 'edward', lastname: 'gomez', date: '1-1-2018'},
-                {firstname: 'vince', lastname: 'vu', date: '1-1-2018'},
-                {firstname: 'michelle', lastname: 'costa', date: '1-1-2018'},
-                {firstname: 'maximus', lastname: 'romulus', date: '1-1-2018'},
-                {firstname: 'elizabeth', lastname: 'okelly', date: '1-1-2018'}
+                {firstname: 'julissa', lastname: 'sanchez', date: '1-2-2018'},
+                {firstname: 'edward', lastname: 'gomez', date: '1-3-2018'},
+                {firstname: 'vince', lastname: 'vu', date: '1-4-2018'},
+                {firstname: 'michelle', lastname: 'costa', date: '1-5-2018'},
+                {firstname: 'maximus', lastname: 'romulus', date: '1-6-2018'},
+                {firstname: 'elizabeth', lastname: 'okelly', date: '1-7-2018'}
             ];
         }
     ]);
